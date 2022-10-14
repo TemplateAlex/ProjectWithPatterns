f50fdc336811e1cbfeab43f4dd5691ed7754e8a0
Mouse Mickey <mickeymouse.kz.ru@outlook.com>
2022-10-14 12:59:54 +0300

    Strategy Pattern first realization

--- ForAizhulov/Patterns/StrategyPattern.cs
+++ ForAizhulov/Patterns/StrategyPattern.cs
@@ -1,6 +1,45 @@
 ﻿namespace ForAizhulov.Patterns
 {
-    public class StrategyPattern
+    internal interface IStrategy
     {
+        string DoSomething();
+    }
+
+    internal class Context
+    {
+        private IStrategy _strategy;
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void DoSomething()
+        {
+            if (this._strategy != null)
+            {
+                this._strategy.DoSomething();
+            }
+        }
+    }
+
+    internal class FirstStrategy: IStrategy
+    {
+        //Создание объекта первой фабрики
+        public string DoSomething()
+        {
+            //Создание первого продукта и вызов его метода
+            return ""; //Возвращение строчки состоящей из HTML кода (пока, хезе чего)
+        }
+    }
+
+    internal class SecondStrategy: IStrategy
+    {
+        //Создание объекта первой фабрики
+        public string DoSomething()
+        {
+            //Создание первого продукта и вызов его метода
+            return ""; //Возвращение строчки состоящей из HTML кода (пока, хезе чего)
+        }
     }
 }
